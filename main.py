9a6d1f3ad894a698c8c49c1390daa69d32bfb0df
kaiquejw <kaiquecapt@gmail.com>
2026-05-28 21:05:11 -0300

    Teste 1

--- main.py
+++ main.py
@@ -18,23 +18,23 @@ CONTAS = [
     {
         "nome": "Juliana", 
         "secret_name": "SESSION_JULIANA",
-        "chat_id": -5108358245,
+        "chat_id": -1002704903786,
         "msg": "Juliana/Jota/R3"
     },
 
         # 21h20 Grupo bate ( domingo ) -1003711333914
-    {
-        "nome": "Giovana", 
-        "secret_name": "SESSION_GIOVANA",
-        "chat_id": -5173808575,
-        "msg": "Giovana - Branco/Raio 6"
-    },
+    #{
+    #    "nome": "Giovana", 
+    #    "secret_name": "SESSION_GIOVANA",
+    #    "chat_id": -1003711333914,
+    #    "msg": "Giovana - Branco/Raio 6"
+    #},
 
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 17
-MINUTO_ALVO = 52
+HORA_ALVO = 19
+MINUTO_ALVO = 00
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
@@ -119,7 +119,7 @@ async def sniper_individual(conta, alvo):
 
 async def main():
     agora = datetime.datetime.now()
-    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=1, microsecond=300000)
+    alvo = agora.replace(hour=HORA_ALVO, minute=MINUTO_ALVO, second=0, microsecond=0) 
     
     print(f"🔥 INICIANDO MODO TURBO ({len(CONTAS)} contas)")
     print(f"🎯 Alvo: {alvo.strftime('%H:%M:%S')}")
