cf7c199ed2f25d9fec1435b55b976e747446f112
kaiquejw <kaiquecapt@gmail.com>
2026-06-03 17:04:22 -0300

    Teste 1

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ CONTAS = [
     {
         "nome": "Thayna", 
         "secret_name": "SESSION_THAYNA",
-        "chat_id": -5188593476,
+        "chat_id": -1003533574600,
         "msg": "Thayna R5 X C7 Marcos"
     },
 
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 17
-MINUTO_ALVO = 3
+HORA_ALVO = 19
+MINUTO_ALVO = 30
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
