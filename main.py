17eeac2e551c4c65441030433328c0aef9d54b4c
kaiquejw <kaiquecapt@gmail.com>
2026-06-18 16:20:04 -0300

    Teste 1

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ TZ = ZoneInfo("America/Sao_Paulo")  # crava horário de Brasília
 
 # ⚠️ AJUSTE PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 16
-MINUTO_ALVO = 12
+MINUTO_ALVO = 22
 SEGUNDO_ALVO = 0
 
 # Começa a martelar a porta um pouco antes do horário.
