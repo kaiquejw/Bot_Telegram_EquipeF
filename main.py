379529807cf146d32c8bf5cd4189a93fbd6d11cd
kaiquejw <kaiquecapt@gmail.com>
2026-06-17 21:27:51 -0300

    Subindo versao vps

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 TZ = ZoneInfo("America/Sao_Paulo")  # crava horário de Brasília
 
 # ⚠️ AJUSTE PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 17
-MINUTO_ALVO = 38
+HORA_ALVO = 18
+MINUTO_ALVO = 29
 SEGUNDO_ALVO = 0
 
 # Começa a martelar a porta um pouco antes do horário.
