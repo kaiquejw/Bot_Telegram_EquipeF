2b898322203de3b6c06277dcf7e82ee674a626ec
kaiquejw <kaiquecapt@gmail.com>
2026-06-25 22:44:47 -0300

     test

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 TZ = ZoneInfo("America/Sao_Paulo")  # crava horário de Brasília
 
 # ⚠️ AJUSTE PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 19
-MINUTO_ALVO = 39
+HORA_ALVO = 20
+MINUTO_ALVO = 0
 SEGUNDO_ALVO = 0
 
 # Começa a martelar a porta um pouco antes do horário.
