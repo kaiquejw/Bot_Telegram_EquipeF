b8fcb8edf479a5432ac5b2be1631280286ec3604
kaiquejw <kaiquecapt@gmail.com>
2026-07-02 19:01:37 -0300

    Teste de todas juntas vps

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 TZ = ZoneInfo("America/Sao_Paulo")  # crava horário de Brasília
 
 # ⚠️ AJUSTE PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 14
-MINUTO_ALVO = 43
+HORA_ALVO = 16
+MINUTO_ALVO = 4
 SEGUNDO_ALVO = 0
 
 # Começa a martelar a porta um pouco antes do horário.
@@ -37,20 +37,20 @@ DESISTIR_APOS_S = 120
 CONTAS = [
     
         #  DOBRA 19:00 🟣 -1002742703600
-    #{
-    #    "nome": "Rafa",
-    #    "secret_name": "SESSION_RAFA",
-    #    "chat_id": -5230687258,
-    #    "msg": "Rafaela X da morte r3"
-    #},
+    {
+        "nome": "Rafa",
+        "secret_name": "SESSION_RAFA",
+        "chat_id": -5230687258,
+        "msg": "Rafaela X da morte r3"
+    },
 
         #  20h00 Grupo da senha DOBRA Flórida Paulista -1002443109385
-    #{
-    #    "nome": "Joice",
-    #    "secret_name": "SESSION_JOICE",
-    #    "chat_id": -5462106210,
-    #    "msg": "Joyce /Guilherme R7 C5"
-    #},
+    {
+        "nome": "Joice",
+        "secret_name": "SESSION_JOICE",
+        "chat_id": -5462106210,
+        "msg": "Joyce /Guilherme R7 C5"
+    },
 
         #  20h00 Grupo da senha DOBRA Flórida Paulista -1002443109385
     {
@@ -61,28 +61,28 @@ CONTAS = [
     },
 
         #  20h00 Grupo senha normal -1003601357589
-    #{
-    #    "nome": "Eduarda",
-    #    "secret_name": "SESSION_EDUARDA",
-    #    "chat_id": -5127442354,
-    #    "msg": "Eduarda x Nego RAIO 5"
-    #},
+    {
+        "nome": "Eduarda",
+        "secret_name": "SESSION_EDUARDA",
+        "chat_id": -5127442354,
+        "msg": "Eduarda x Nego RAIO 5"
+    },
 
         #  20h30 Senha Grupo Normal -1003927816412
-    #{
-    #    "nome": "Juliana",
-    #    "secret_name": "SESSION_JULIANA",
-    #    "chat_id": -5108358245,
-    #    "msg": "Juliana/Jota/R3"
-    #},
+    {
+        "nome": "Juliana",
+        "secret_name": "SESSION_JULIANA",
+        "chat_id": -5108358245,
+        "msg": "Juliana/Jota/R3"
+    },
 
         #  20h45 Senha Grupo Preferencial -1003552682244
-    #{
-    #    "nome": "Anne",
-    #    "secret_name": "SESSION_ANNE",
-    #    "chat_id": -1004440363120,
-    #    "msg": "Anny x JB R2"
-    #},
+    {
+        "nome": "Anne",
+        "secret_name": "SESSION_ANNE",
+        "chat_id": -1004440363120,
+        "msg": "Anny x JB R2"
+    },
 
 ]
 
