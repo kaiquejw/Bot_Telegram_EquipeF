70c1e1251f9f20d84e9f4f3ab084dc61213beb7c
kaiquejw <kaiquecapt@gmail.com>
2026-07-09 20:36:19 -0300

    produção jAKEEEEE

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ TZ = ZoneInfo("America/Sao_Paulo")  # crava horário de Brasília
 
 # ⚠️ AJUSTE PARA O DIA DA SENHA ⚠️
 HORA_ALVO = 20
-MINUTO_ALVO = 36
+MINUTO_ALVO = 38
 SEGUNDO_ALVO = 0
 
 # Começa a martelar a porta um pouco antes do horário.
