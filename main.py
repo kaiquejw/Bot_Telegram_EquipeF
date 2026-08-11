76799fd97ce5238e6ebe9e6d22fc497eeaa2f394
kaiquejw <kaiquecapt@gmail.com>
2026-08-11 11:07:48 -0300

    Teste 1 isabelaaa

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get('TELEGRAM_API_HASH')
 TZ = ZoneInfo("America/Sao_Paulo")  
 
 
-HORA_ALVO = 20
-MINUTO_ALVO = 30
+HORA_ALVO = 11
+MINUTO_ALVO = 10
 SEGUNDO_ALVO = 0
 
 ANTECIPACAO_S = 0.0
@@ -34,32 +34,47 @@ DESISTIR_APOS_S = 120
 CONTAS = [
 
 
+        #  19h00 Senha Normal (Dobra) 19:00 HS -100441725253
+    {
+        "nome": "Isabella",
+        "secret_name": "SESSION_ISABELLA",
+        "chat_id": -5174570129,
+        "msg": "Isa/ NK R3"
+    },
 
+        #  19h00 Senha Normal (Dobra) 19:00 HS -100441725253
+    {
+        "nome": "Isa",
+        "secret_name": "SESSION_ISA",
+        "chat_id": -5174570129,
+        "msg": "Isa/ NK R3 teste"
+    },
 
 
         #  20h30 Senha Grupo Normal -1003927816412
-    {
-        "nome": "Juliana",
-        "secret_name": "SESSION_JULIANA",
-        "chat_id": -1003927816412,
-        "msg": "Juliana/Jota/R3"
-    },
+    #{
+    #    "nome": "Juliana",
+    #    "secret_name": "SESSION_JULIANA",
+    #    "chat_id": -1003927816412,
+    #    "msg": "Juliana/Jota/R3"
+    #},
 
         #  20h30 Senha Grupo Normal -1003927816412
-    {
-        "nome": "Angelica",
-        "secret_name": "SESSION_ANGELICA",
-        "chat_id": -1003927816412,
-        "msg": "Angelica x tales r3"
-    },
+    #{
+    #    "nome": "Angelica",
+    #    "secret_name": "SESSION_ANGELICA",
+    #    "chat_id": -1003927816412,
+    #    "msg": "Angelica x tales r3"
+    #},
 
         #  20h30 Senha Grupo Normal -1003927816412
-    {
-        "nome": "Andreia",
-        "secret_name": "SESSION_ANDREIA",
-        "chat_id": -1003927816412,
-        "msg": "Andreia/Giovanna X Fagner Raio 1"
-    },
+    #{
+    #    "nome": "Andreia",
+    #    "secret_name": "SESSION_ANDREIA",
+    # 
+    #    "chat_id": -1003927816412,
+    #    "msg": "Andreia/Giovanna X Fagner Raio 1"
+    #},
 
 ]
 
