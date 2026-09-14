8596755467267761c817478a30a6d58b631d0764
kaiquejw <kaiquecapt@gmail.com>
2026-09-14 18:48:33 -0300

    Realiando tst velc

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get("TELEGRAM_API_HASH")
 TZ = ZoneInfo("America/Sao_Paulo")
 
 
-HORA_ALVO = 16
-MINUTO_ALVO = 35
+HORA_ALVO = 18
+MINUTO_ALVO = 55
 SEGUNDO_ALVO = 0
 
 ANTECIPACAO_S = 0.0
@@ -33,16 +33,9 @@ DESISTIR_APOS_S = 120
 CONTAS = [
     #  20h45 Senha Grupo Preferencial -1003552682244
     {
-        "nome": "Anne",
-        "secret_name": "SESSION_ANNE",
-        "chat_id": -1003552682244,
-        "msg": "Anny x JB R2",
-    },
-    #  20h45 Senha Grupo Preferencial -1003552682244
-    {
         "nome": "Jake",
         "secret_name": "SESSION_JAKE",
-        "chat_id": -1003552682244,
+        "chat_id": -5186073583,
         "msg": "Jakeline x Daniel raio 3",
     },
 ]
