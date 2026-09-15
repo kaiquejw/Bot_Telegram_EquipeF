66d45046cfce8d3a97abb608eca79e0035736d1e
kaiquejw <kaiquecapt@gmail.com>
2026-09-15 12:21:34 -0300

    TEste terca

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ API_HASH = os.environ.get("TELEGRAM_API_HASH")
 TZ = ZoneInfo("America/Sao_Paulo")
 
 
-HORA_ALVO = 18
-MINUTO_ALVO = 55
+HORA_ALVO = 13
+MINUTO_ALVO = 30
 SEGUNDO_ALVO = 0
 
 ANTECIPACAO_S = 0.0
